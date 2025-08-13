a86e6c381f52d919cd8f54fced82e1bd1c675bac
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2025-08-13 15:22:42 +0200

    update software (see README)

--- skrypy/NodeEditor/modules/Environment/ssh_execution.py
+++ skrypy/NodeEditor/modules/Environment/ssh_execution.py
@@ -1,45 +1,3 @@
-class ssh_diagram_execution():
-    def __init__(self, diagram=['path'], host_skrypy_path='path', host_name='', host_password='', **options):
-        from subprocess import call
-        import os
-
-        if not host_password or host_password == 'None':
-            print("connection abandoned")
-            self.output = ''
-        else:
-            if not isinstance(diagram, list):
-                diagram = [diagram]
-            path_ssh_cmd_file = os.path.join(os.path.expanduser('~'), '.skrypy', 'ssh_command.sh')
-            diagram = str(diagram).replace(' ', '')
-            n_cpu = 1
-            pre_exec, opx = '', ''
-            if 'cpu_number' in options:
-                n_cpu = options['cpu_number']
-            if 'pre_exec' in options:
-                pre_exec = options['pre_exec']
-            if 'X11_forwarding' in options:
-                if options['X11_forwarding']:
-                    opx = '-X'
-            with open(path_ssh_cmd_file, 'w') as fssh:
-                fssh.write(pre_exec+"\n")
-                fssh.write("cd {}\n".format(host_skrypy_path))
-                fssh.write("source bin/activate\n")
-                fssh.write("cd skrypy\n")
-                fssh.write("python3 Execution_ssh.py {} {}\n".format(diagram, n_cpu))
-                fssh.write("deactivate\n")
-                fssh.write("echo\n")
-                fssh.write("echo \"Finished.. you can close this window\"\n")
-                fssh.write("echo \n")
-                fssh.write("exit\n")
-            os.system(f"gnome-terminal -e 'bash -c \"sshpass -p " + host_password.strip() + " ssh " + opx +
-                      " " + host_name + " < ~/.skrypy/ssh_command.sh; bash\"'")
-
-    def stdout(self: 'str'):
-        return self.output
-
-######################################################################################################################
-
-
 class ssh_file_dir_exists():
     def __init__(self, path='path', host_name='', host_password=''):
         from subprocess import Popen, PIPE
@@ -64,3 +22,31 @@ class ssh_list_files_in_directory():
 
     def exists(self: 'str'):
         return self.answ
+    
+######################################################################################################################
+
+
+# class ssh_remove_files_directory():
+#     def __init__(self, path_type="enumerate(('file', 'directory'))", paths='path', host_name='', host_password=''):
+#         from subprocess import Popen, PIPE
+#
+#         if not isinstance(paths, list):
+#             source = [paths]
+#         else:
+#             source = paths
+#
+#         for src in source:
+#             if path_type == 'directory':
+#                 exec_cli = "rm -r {}".format(src)
+#             else:
+#                 exec_cli = "rm {}".format(src)
+#             cmd = ['sshpass', '-p', host_password, 'ssh', host_name, exec_cli]
+#             print(" ".join(cmd[3:]))
+#             p = Popen(cmd, stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
+#             # self.output = p.stdout.read().decode()
+#             p.communicate()
+#             p.wait()
+#             if p.returncode == 0:
+#                 print('{} deleted'.format(src))
+#             else:
+#                 print('deletion error for {}'.format(src))
