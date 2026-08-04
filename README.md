c287ddd1dd2ae27aac6f799cff29fc8125ead6ae
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2026-08-04 14:56:34 +0200

    update software (see README)

--- README.md
+++ README.md
@@ -11,6 +11,11 @@ A documentation is available here : [https://montigno.github.io/skrypy-pyqt5](ht
 
 <p></p>
 
+	04/08/2026: version 26.08.04a
+	- Some bugs fixed.
+
+<p></p>
+
 	31/07/2026: version 26.07.31a
 	- Bug with the Skrypy closing dialog window resolved.
 
--- skrypy-pyqt5/NodeEditor/python/Diagram_Editor.py
+++ skrypy-pyqt5/NodeEditor/python/Diagram_Editor.py
@@ -5661,7 +5661,7 @@ class LoadCodeScript:
             if unitScript + ':' in tmpout:
                 tmpIn = tmpout[tmpout.index(':') + 1:]
                 tmpVal = val[0:val.index('#Node#')]
-                if tmpVal[0] is 'A':
+                if tmpVal[0] == 'A':
                     tmpConstName = tmpVal[0:-1]
                     tmpIt = editor.listItems[editor.currentTab][tmpConstName]
                     tmpVal = editor.listConstants[editor.currentTab][tmpConstName][1]
@@ -5678,7 +5678,7 @@ class LoadCodeScript:
                     if type(tmpIt).__name__ == 'Checkbox':
                         tmpVal = [i[0:-1] for i in tmpVal if '*' in i]
                     tmpVal = repr(tmpVal)
-                elif tmpVal[0] is 'W':
+                elif tmpVal[0] == 'W':
                     tmpConstName = tmpVal[0:-1]
                     tmpVal = editor.listItems[editor.currentTab][tmpConstName].widget.state['selected_paths']
                     tmpVal = repr(tmpVal)
